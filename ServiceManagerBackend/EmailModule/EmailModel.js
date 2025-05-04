328bed17d26ac84e66ceaaba9fe5a4461f714d01
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-05-04 20:25:47 +0100

    feat(EmailModule): make placeholder for EmailModule

---
+++ ServiceManagerBackend/EmailModule/EmailModel.js
@@ -0,0 +1,41 @@
+import Sequelize from 'sequelize';                   // to use the sequelize library
+import sequelize from '../DatabaseModule/ConnectDB'; // to connect to the database
+
+
+const Email = sequelize.define('emails', {
+    email_id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    title: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    },
+
+    content: {
+        type: Sequelize.STRING(2000),
+        allowNull: false
+    },
+
+    source_user_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'user_id'
+        }
+    },
+
+    destination_user_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'user_id'
+        }
+    }
+})
+
+export default Email;
