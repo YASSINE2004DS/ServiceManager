34fdcd20f6ceb5203337793f82d065e5d616b497
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-05-04 20:25:21 +0100

    feat(AgencyModule): make placeholder for the AgencyModule

---
+++ ServiceManagerBackend/AgencyModule/AgencyModel.js
@@ -0,0 +1,34 @@
+import Sequelize from 'sequelize'; // to use the sequelize library
+import sequelize from '../DatabaseModule/ConnectDB'; // to connect to the database
+
+// define the Agency model
+const Agency = sequelize.define('agencies', {
+
+    agency_id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    name: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+    },
+
+    start_time: {
+        type: Sequelize.TIME,
+        allowNull: false
+    },
+
+    end_time: {
+        type: Sequelize.TIME,
+        allowNull: false
+    },
+
+    adress: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    },
+});
+
+export default Agency;
