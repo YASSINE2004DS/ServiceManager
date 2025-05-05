f1b0a97bb50f4f68b3f37752ecf9989a6d8dd1a4
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-05-05 20:20:18 +0100

    feat(UserModule): make first Schema for user

---
+++ ServiceManagerBackend/UserModule/UserValidator.js
@@ -0,0 +1,36 @@
+import Joi from 'joi';
+
+const createUserSchema = Joi.object(
+{
+    first_name: Joi.string().min(2).max(50).required(),
+
+    last_name:  Joi.string().min(2).max(50).required(),
+
+    email:      Joi.string().email().required(),
+
+    password:   Joi.string().min(5).max(200).required(),
+
+    role:       Joi.string().valid('Admin', 'User').default('User'),
+});
+
+const updateUserSchema = Joi.object(
+{
+    first_name: Joi.string().min(2).max(50),
+
+    last_name:  Joi.string().min(2).max(50),
+
+    email:      Joi.string().email(),
+
+    password:   Joi.string().min(5).max(200),
+
+    role:       Joi.string().valid('Admin', 'User'),
+}
+
+
+);
+
+
+export default {
+    createUserSchema,
+    updateUserSchema
+};
