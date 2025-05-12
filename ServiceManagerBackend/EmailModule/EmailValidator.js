8c144cb879d3ece1d66abaa2b31488a6473388d5
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-05-12 20:15:34 +0100

    feat(EmailModule): update the schema of validation

--- ServiceManagerBackend/EmailModule/EmailValidator.js
+++ ServiceManagerBackend/EmailModule/EmailValidator.js
@@ -4,8 +4,7 @@ import Joi from 'joi';
 const createEmailSchema = Joi.object({
     title: Joi.string().max(100).required(),
     content: Joi.string().max(2000).required(),
-    source_user_id: Joi.number().integer().required(),
-    destination_user_id: Joi.number().integer().required()
+    destination_email: Joi.string().email().required()
 });
 
 
