46f54eafe4f35ab44104c2f73f5a476ee4d7531f
YASSINE2004DS <eljarjiniyassine1@gmail.com>
2025-06-05 19:27:06 +0100

    version__05/06/25

--- ServiceManagerFrontend/src/PagesAdmin/PageAcceuil/PageAdmin.jsx
+++ ServiceManagerFrontend/src/PagesAdmin/PageAcceuil/PageAdmin.jsx
@@ -63,51 +63,58 @@ export default function AdminDashboard() {
                 return <GestionAgences />;
             case 'comptesNonActive':
                 return <GestionComptesNonActive />;
+            case 'statistiques':
+                  return <StatistiquesServives />
             default:
                 return null;
         }
     };
 
-  window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [isSidebarOpen]);
 
-  // Gérer le scroll du body quand le sidebar est ouvert/fermé sur mobile
-  useEffect(() => {
-    if (window.innerWidth <= 768) {
-      document.body.style.overflow = isSidebarOpen ? 'hidden' : '';
-    } else {
-      document.body.style.overflow = ''; // S'assurer que le scroll est activé sur desktop
-    }
-  }, [isSidebarOpen]);
-
-
-  const renderContent = () => {
-    switch (Page) {
-      case 'utilisateurs':
-        return <GestionUtilisateurs />;
-      case 'sections':
-        return <GestionSections />;
-      case 'interventions':
-        return <GestionInterventions />;
-      case 'agences':
-        return <GestionAgences />;
-      case 'statistiques':
-         return <StatistiquesServives />
-      default:
-        return null;
-    }
-  };
 
   return (
-    <div className="Container-globales">
+      <div className="Container-globales">
       <PageHeader />
     <div className="dashboard-layout">
         <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
             <div className="sidebar-header">
                 <h2>Admin</h2>
             </div>
-        </div>
+            <nav className="sidebar-nav">
+                <ul>
+                {menuItems.map((item) => (
+                    <li  key={item.key}>
+                          <a 
+                          href={`/admin/${item.key}`}
+                          className={`nav-item  ${Page === item.key ? 'active' : ''}`}>{item.label}
+                          </a>
+                    </li>
+
+                      ))}
+                </ul>
+            </nav>
+        </aside>
+
+
+
+        {/* // style={{marginLeft:0}} */}
+        <main className="main-content">  
+            <button   className={`menuToggle ${isSidebarOpen ? 'active' : ''}`}
+                      onClick={toggleSidebar}
+                      aria-label="Toggle Menu"
+            >
+
+                <svg style={{height:30}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" classNameName="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
+          {/* <h1 classNameName="main-title"></h1>
+          <div classNameName="content-card">{renderContent()}</div> */}
+          {renderContent()}
+        </main>
+      </div>
+  </div>
+
     );
 }
 
